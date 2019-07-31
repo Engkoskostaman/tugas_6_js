b9299a1ee2f254d468422fbcfb57aaca0e5c4792
Engkos <engkoskostaman97@gmail.com>
2019-08-01 01:58:55 +0700

    tugas 6  js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas6.js/index.js
@@ -0,0 +1,10 @@
+function perulangan(){
+for (var i = 1; i <= 20; i++)
+if ((i % 2) == 0) {
+console.log(i + ' adalah bilangan genap ');
+}
+else if ((i % 2) !== 0)
+{
+console.log(i + ' ');}
+};
+perulangan();
